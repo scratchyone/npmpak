c361e0d8ae9f62e00b36330ad6b4ed3778b64443
Scratchyone <scratchywon@gmail.com>
2018-07-04 10:56:11 -0400

    Gets latest version

--- main.js
+++ main.js
@@ -7,70 +7,94 @@ var fs = require('fs');
 app.get('/', (req, res) => res.send('HOMEPAGE NOT CONFIGURED'));
 
 app.get('/:href(*)', (req, res) => {
+  res.header('Content-Type', 'application/javascript');
   req.params[0] = req.params[0].replace(/\./g, '');
-  fs.readFile(
-    'cache/' +
-      req.params[0].replace(/\//g, '-') +
-      '-' +
-      Object.keys(req.query)[0] +
-      '.js',
-    (err, data) => {
-      if (err) {
-        install(
-          {
-            dependencies: [req.params[0].replace(/[/].*/g, '')]
-          },
-          function(err) {
-            if (err) {
-              console.error('There was an error installing.');
-              res.send('//Failed, ' + err);
-            } else {
-              child_process.execFile(
-                'browserify',
-                [
-                  '--require',
-                  req.params[0],
-                  ...(Object.keys(req.query)[0]
-                    ? ['--standalone', Object.keys(req.query)[0]]
-                    : [])
-                ],
-                function(error, stdout, stderr) {
-                  fs.writeFile(
-                    'cache/' +
-                      req.params[0].replace(/\//g, '-') +
-                      '-' +
-                      Object.keys(req.query)[0] +
-                      '.js',
-                    stdout,
-                    function(err) {
-                      res.sendFile(
+  console.log(req.params[0].replace(/[/].*/g, ''));
+  child_process.execFile(
+    'npm',
+    ['show', req.params[0].replace(/[/].*/g, ''), 'version'],
+    (pkgerr, pkgversion, pkgerror) => {
+      fs.readFile(
+        'cache/' +
+          req.params[0].replace(/\//g, '-') +
+          '-' +
+          Object.keys(req.query)[0] +
+          '-' +
+          pkgversion.replace(/\./g, '') +
+          '.js',
+        (err, data) => {
+          if (err) {
+            install(
+              {
+                dependencies: [req.params[0].replace(/[/].*/g, '')]
+              },
+              function(err) {
+                if (err) {
+                  console.error('There was an error installing.');
+                  res.send('//Failed, ' + err);
+                } else {
+                  console.log([
+                    '--require',
+                    req.params[0],
+                    ...(Object.keys(req.query)[0]
+                      ? ['--standalone', Object.keys(req.query)[0]]
+                      : [])
+                  ]);
+                  child_process.execFile(
+                    'browserify',
+                    [
+                      '--require',
+                      req.params[0],
+                      ...(Object.keys(req.query)[0]
+                        ? ['--standalone', Object.keys(req.query)[0]]
+                        : [])
+                    ],
+                    function(error, stdout, stderr) {
+                      console.log(stdout);
+                      fs.writeFile(
                         'cache/' +
                           req.params[0].replace(/\//g, '-') +
                           '-' +
                           Object.keys(req.query)[0] +
+                          '-' +
+                          pkgversion.replace(/\./g, '') +
                           '.js',
-                        { root: __dirname }
+                        stdout,
+                        function(err) {
+                          res.sendFile(
+                            'cache/' +
+                              req.params[0].replace(/\//g, '-') +
+                              '-' +
+                              Object.keys(req.query)[0] +
+                              '-' +
+                              pkgversion.replace(/\./g, '') +
+                              '.js',
+                            { root: __dirname }
+                          );
+                          if (err) {
+                            return console.log(err);
+                          }
+                        }
                       );
-                      if (err) {
-                        return console.log(err);
-                      }
                     }
                   );
                 }
-              );
-            }
+              }
+            );
+          } else {
+            res.sendFile(
+              'cache/' +
+                req.params[0].replace(/\//g, '-') +
+                '-' +
+                Object.keys(req.query)[0] +
+                '-' +
+                pkgversion.replace(/\./g, '') +
+                '.js',
+              { root: __dirname }
+            );
           }
-        );
-      } else {
-        res.sendFile(
-          'cache/' +
-            req.params[0].replace(/\//g, '-') +
-            '-' +
-            Object.keys(req.query)[0] +
-            '.js',
-          { root: __dirname }
-        );
-      }
+        }
+      );
     }
   );
 });
