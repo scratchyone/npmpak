8562e70e177acf437471bf0807fc8bb8fe9d7406
Scratchyone <scratchywon@gmail.com>
2018-07-04 11:37:12 -0400

    Good font

--- main.js
+++ main.js
@@ -6,23 +6,23 @@ var fs = require('fs');
 
 app.get('/', (req, res) =>
   res.send(
-    '<h1><a id="NPMPak_0"></a>NPMPak</h1>\n\
-<p>Easy Browserify CDN for fast prototyping. It supports bundle and standalone.</p>\n\
-<h5><a id="How_does_it_work_3"></a>How does it work?</h5>\n\
-<ol>\n\
-<li>It runs npm show</li>\n\
-<li>It checks if up to date cached version exists.</li>\n\
-<li>If it doesn’t, it downloads the package from npm.</li>\n\
-<li>Then it runs browserify on the package.</li>\n\
-<li>It saves the result to file, and sends.</li>\n\
-</ol>\n\
-<h2><a id="API_10"></a>API</h2>\n\
-<h4><a id="GET_package_12"></a>GET /:package</h4>\n\
-<p>Get module in bundled mode.<br>\n\
-Demo: <a href="https://npmpak.scratchyone.com/uuid">https://npmpak.scratchyone.com/uuid</a></p>\n\
-<h4><a id="GET_packagename_16"></a>GET /:package?:name</h4>\n\
-<p>Get module in standalone mode.<br>\n\
-Demo: <a href="https://npmpak.scratchyone.com/uuid?uuidv1">https://npmpak.scratchyone.com/uuid?uuidv1</a></p>'
+    `<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"><style>*{font-family: 'Roboto', sans-serif;}</style><h1><a id="NPMPak_0"></a>NPMPak</h1>
+<p>Easy Browserify CDN for fast prototyping. It supports bundle and standalone.</p>
+<h5><a id="How_does_it_work_3"></a>How does it work?</h5>
+<ol>
+<li>It runs npm show</li>
+<li>It checks if up to date cached version exists.</li>
+<li>If it doesn’t, it downloads the package from npm.</li>
+<li>Then it runs browserify on the package.</li>
+<li>It saves the result to file, and sends.</li>
+</ol>
+<h2><a id="API_10"></a>API</h2>
+<h4><a id="GET_package_12"></a>GET /:package</h4>
+<p>Get module in bundled mode.<br>
+Demo: <a href="https://npmpak.scratchyone.com/uuid">https://npmpak.scratchyone.com/uuid</a></p>
+<h4><a id="GET_packagename_16"></a>GET /:package?:name</h4>
+<p>Get module in standalone mode.<br>
+Demo: <a href="https://npmpak.scratchyone.com/uuid?uuidv1">https://npmpak.scratchyone.com/uuid?uuidv1</a></p>`
   )
 );
 
